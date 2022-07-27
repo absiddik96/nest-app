84867adfe321eb225be554667413e6b98fab92b0
ab siddik <ab@siddik.me>
2022-07-28 00:56:10 +0600

    working on user crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,65 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from "@nestjs/common";
 import { UserService } from "./user.service";
 import { StoreDto } from "./dto/store.dto";
+import { UpdateDto } from "./dto/update.dto";
 
 @Controller("users")
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get()
-  index() {
+  @Get("/")
+  async index() {
     return this.userService.index();
   }
 
-  @Post()
-  store(@Body() storeDto: StoreDto) {
-    this.userService.store(storeDto);
+  @Post("/")
+  async store(@Body() storeDto: StoreDto) {
+    const user = await this.userService.store(storeDto);
 
     return {
       message: "User create successfully",
+      status: 200,
+      user,
+    };
+  }
+
+  @Get("/:userId")
+  async show(@Param("userId") userId: string) {
+    return this.userService.show(userId);
+  }
+
+  @Put("/:userId")
+  async update(@Param("userId") userId: string, @Body() updateDto: UpdateDto) {
+    const user = await this.userService.update(userId, updateDto);
+
+    if (!user) {
+      return {
+        message: "User mpt successfully",
+        status: 404,
+      };
+    }
+
+    return {
+      message: "User updated successfully",
+      status: 200,
+      user,
+    };
+  }
+
+  @Delete("/:userId")
+  async destroy(@Param("userId") userId: string) {
+    await this.userService.destroy(userId);
+
+    return {
+      message: "User deleted successfully",
       status: 200,
     };
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { StoreDto } from "./dto/store.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { User, UserDocument } from "./user.schema";
+import { Model } from "mongoose";
+import { UpdateDto } from "./dto/update.dto";
 
 @Injectable()
 export class UserService {
-  users = [
-    {
-      name: "name",
-      email: "email",
-    },
-  ];
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  index() {
-    return this.users;
+  async index(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 
-  store(storeDto: StoreDto) {
-    this.users.push(storeDto);
+  async store(storeDto: StoreDto): Promise<User> {
+    return new this.userModel(storeDto).save();
+  }
+
+  async show(userId: string): Promise<User> {
+    console.log(userId);
+    return this.userModel.findById(userId);
+  }
+
+  async update(userId: string, updateDto: UpdateDto): Promise<User> {
+    return this.userModel.findByIdAndUpdate(userId, updateDto);
+  }
+
+  async destroy(userId: string): Promise<User> {
+    return this.userModel.findByIdAndRemove(userId);
   }
 }
